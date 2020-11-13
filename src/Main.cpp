829b415e35b902a55df1358739bbdd4e0e4169b0
MathWintruff <mathwintruffac@gmail.com>
2020-11-13 19:39:20 -0300

    minor adjust to translation, compile error fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #pragma region Variables
 //-------- Inputs --------------
 const int ThresholdTrimpot = 34;
@@ -102,19 +104,6 @@ void IoInicialization(){
   digitalWrite(InternalLed, HIGH);
 }
 
-void TouchRead(){
-  int touchReadSum = 0, touchReadResult = 0;
-  for(int i = 0; i < 100; i++){
-    touchReadSum += touchRead(T0);
-  }
-  touchReadResult = (touchReadSum / 100);
-  
-  if(touchReadResult < TresholdValue()){
-    ShelfLightState();
-    delay(1000);
-  }
-}
-
 void ShelfLightState(){
   static boolean state = false;
   state = !state;
@@ -126,6 +115,19 @@ int TresholdValue(){
   int Threshold = analogRead(ThresholdTrimpot);
   Threshold = map(Threshold, 0, 4095, 0, 100);
   return Threshold;
+}
+
+void TouchRead(){
+  int touchReadSum = 0, touchReadResult = 0;
+  for(int i = 0; i < 100; i++){
+    touchReadSum += touchRead(T0);
+  }
+  touchReadResult = (touchReadSum / 100);
+  
+  if(touchReadResult < TresholdValue()){
+    ShelfLightState();
+    delay(1000);
+  }
 }
 
 #pragma endregion
